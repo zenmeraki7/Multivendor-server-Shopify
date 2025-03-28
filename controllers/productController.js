03eddcf25deaf1f4c181facb598bb3f97c3b97ee
Abhishek7356 <ksabhishek32@gmail.com>
2025-03-28 11:02:41 +0530

    updated

--- controllers/productController.js
+++ controllers/productController.js
@@ -330,6 +330,7 @@ export const getAllProducts = async (req, res) => {
     const products = await Product.find(query)
       .populate("vendor")
       .populate("images")
+      .populate("variants")
       .skip(skip)
       .limit(limit)
       .sort({ createdAt: -1 });
